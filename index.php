39bde279232ce2177721927ad515d337bf013a8b
haigef <thiago@sotem.com.br>
2016-03-29 22:15:38 -0300

    Added files via upload

---
+++ index.php
@@ -0,0 +1,36 @@
+﻿<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>index</title>
+<link rel= "stylesheet" tipe= "text/css" href="css/default.css">
+</head>
+<body>
+    <div class="head" >
+    	<div class="login">
+        	<form action="#" method="">
+            	<br>
+                	<input class="nomelogin" type="text" name="nomelogin">
+                    	<br>
+                    <input class="senhalogin" type="password" name="senha">
+                    <a href="#" class="resetsenha">Esqueci minha senha</a>
+                    <button class="botaologin" type="submit">Ok</button>
+             </form>
+     	 </div>
+    </div>
+
+    <div class="nav">
+        <button id="buttonMenu1"><font class="fontMenu">B&aacute;sico</font></button>
+        <button class="buttonMenu"><font class="fontMenu">Avan&ccedil;ado</font></button>
+        <button class="buttonMenu"><font class="fontMenu">Relat&oacute;rios</font></button>
+        <button class="buttonMenu"><font class="fontMenu">Configura&ccedil;&otilde;es</font></button>
+    </div>
+    <br>
+    <div class="asside"><br><font class="fontMenu">&nbsp; &nbsp; &nbsp; Cadastros ></font ></div>
+    <div class="section">	
+         <form action="#" method="">
+         	<input class="busca" type="search" name="busca">
+          </form>
+    </div>
+</body>
+</html>
